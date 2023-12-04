dad09988f6f58e67ae239f75f395afc3cd589d49
ethandmd <ethandmd@protonmail.com>
2023-12-04 02:28:20 -0800

    Refactor into PVH platform module.
    I found better documentation on the Xen PVH Boot ABI (aka I found linux/arch/x86/platform/pvh/head.S)
    and refactored boot code into an assembly _start entry point and a _rust_start entry point. This way
    the assembly code can setup the GDT and Stack at the least before jumping to long mode. Some of this
    may be able to get lifted up into Rust (I know we can play with GDT, page tables, etc using the x86_64
    crate) however not having a stack and hvm_start_info pointer in %ebx means it's easier to start in asm.

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,35 @@
 #![no_std]
 #![no_main]
-//#![feature(lang_items)]
+#![allow(internal_features)]
+#![feature(lang_items)]
 
 pub mod platform;
 
-use core::arch::{asm, global_asm};
+use core::arch::global_asm;
 use core::panic::PanicInfo;
-// Still need this?
-//#[lang = "eh_personality"]
-//extern "C" fn eh_personality() {}
 
-fn kernel_init(_start_info: &platform::pvh::StartInfo) {}
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
 
 // Reference: https://github.com/rust-osdev/bootloader/blob/a1b2eb88fd365fa7b4b35e29495076f22932cb7f/api/src/lib.rs#L110
 #[macro_export]
 macro_rules! entry_point {
     ($entry:path) => {
+        // Include in order to get _start symbol for kernel entry.
+        global_asm!(include_str!("platform/pvh/start.S"));
+
         #[panic_handler]
         fn panic(_info: &PanicInfo) -> ! {
             loop {}
         }
-
-        extern "C" {
-            //static STACK_TOP: u64;
-        }
-
         // PVH Boot Reference: https://xenbits.xen.org/docs/4.6-testing/misc/pvh.html
-        // rust-vmm xen: https://github.com/rust-vmm/xen-sys/blob/main/xen/src/lib.rs
-        //elfnote!(6, "asciz", "\"unifire\"");
-        //elfnote!(7, "asciz", "\"0.1.0\"");
-        //elfnote!(8, "asciz", "\"generic\"");
-        //elfnote!(5, "asciz", "\"xen-3.0\"");
-        //elfnote!(10, "asciz", "\"!writable_page_tables|pae_pgdir_above_4gb\"");
-        //elfnote!(9, "asciz", "\"yes\"");
-        elfnote!(18, "quad", "0x100000");
+        // Linux PVH Boot: https://github.com/torvalds/linux/blob/master/arch/x86/platform/pvh/head.S
+        // CH uses linux-loader::loader::Elf::load() to load the kernel ELF image.
+        // load() uses parse_elf_note() which only checks for XEN_ELFNOTE_PHYS32_ENTRY.
+        elfnote!(18, "quad", "_start"); // XEN_ELFNOTE_PHYS32_ENTRY.
 
-        #[export_name = "_start"] // Review: no_mangle vs export_name
-        pub extern "C" fn __impl_start() -> ! {
-            // Might need to setup our stack using STACK_TOP symbol from linker script
-            // instead of using the stack the loader provides...
-            // %rsp points to top of initial single page stack.
-            // %rsi points to start_info structure.
-            // load start_info address from %rsi onto stack and call kernel_init.
-            // load start_info address from %rsi into rust variable:
-            let start_info: &platform::pvh::StartInfo = unsafe {
-                let start_info: u64;
-                asm!(
-                    "mov rsi, {}",
-                    out(reg) start_info
-                );
-                &*(start_info as *const platform::pvh::StartInfo)
-            };
-            kernel_init(start_info);
+        #[no_mangle] // Review: no_mangle vs export_name
+        pub extern "C" fn _rust_start() -> ! {
             // Validate entry point function signature.
             let f: fn() -> ! = $entry;
             f();
--- src/platform/pvh/mod.rs
+++ src/platform/pvh/mod.rs
@@ -1,5 +1,14 @@
 #![allow(dead_code)]
 
+//_pa(x) ((x) - _KERNEL_BASE) /* from Linux */
+pub static PVH_GDT_ENTRY_CS: u32 = 1;
+pub static PVH_GDT_ENTRY_DS: u32 = 2;
+pub static PVH_CS_SEL: u32 = PVH_GDT_ENTRY_CS * 8;
+#[no_mangle]
+pub static PVH_DS_SEL: u32 = PVH_GDT_ENTRY_DS * 8;
+#[no_mangle]
+pub static PAE_BIT: u32 = 1 << 5;
+
 // Stolen from: linux/include/xen/interface/elfnote.h
 // The notes should live in a SHT_NOTE segment and have "Xen" in the
 // name field.
@@ -144,18 +153,21 @@ pub const XEN_ELFNOTE_PHYS32_ENTRY: u32 = 18;
 // The number of the highest elfnote defined.
 pub const XEN_ELFNOTE_MAX: u32 = XEN_ELFNOTE_PHYS32_ENTRY;
 
+pub const PVH_BOOT_MAGIC: u32 = 0x336ec578;
+
 /// Reference: https://github.com/xen-project/xen/blob/master/xen/include/public/arch-x86/hvm/start_info.h
+/// CH sets at address 0x6000.
 pub struct StartInfo {
     pub magic: u32,          // == 0x336ec578
-    pub version: u32,        // == version of this struct
+    pub version: u32,        // == version of this struct. PVH should be 1.
     pub flags: u32,          // SIF_xxx flags
-    pub nr_modules: u32,     // number of modules passed to the kernel
-    pub modlist_paddr: u32,  // physical address of an array of hvm_modlist_entry
-    pub cmdline_paddr: u32,  // physical address of the command line
-    pub rsdp_paddr: u32,     // physical address of the RSDP ACPI data struct
-    pub memmap_paddr: u32,   // physical address of the memory map
+    pub nr_modules: u32,     // number of modules passed to the kernel. 0 if no modules.
+    pub modlist_paddr: u32, // physical address of an array of hvm_modlist_entry. PVH should have it at 0x6040.
+    pub cmdline_paddr: u32, // physical address of the command line
+    pub rsdp_paddr: u32,    // physical address of the RSDP ACPI data struct
+    pub memmap_paddr: u32,  // physical address of the memory map. PVH should have it at 0x7000.
     pub memmap_entries: u32, // nr entries in memmap table. 0 if no memmap provided.
-    pub reserved: u32,       // must be zero.
+    pub reserved: u32,      // must be zero.
 }
 
 /// Reference: https://github.com/xen-project/xen/blob/master/xen/include/public/arch-x86/hvm/start_info.h
@@ -173,6 +185,8 @@ pub struct HvmMemMapTableEntry {
     pub mapping_type: u32, // type of memory range
     pub reserved: u32,     // must be zero
 }
+
+// CH PVH Setup Reference: https://github.com/cloud-hypervisor/cloud-hypervisor/blob/5f89461a7e95f937733cefa4d2854f98873e3f88/arch/src/x86_64/mod.rs#L929
 
 #[macro_export]
 macro_rules! elfnote {
