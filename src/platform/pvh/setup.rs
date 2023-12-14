103ec1ee422ade8be3133eaf490b33306103062e
ethandmd <ethandmd@protonmail.com>
2023-12-14 12:34:45 -0800

    Use memtable iter

--- src/platform/pvh/setup.rs
+++ src/platform/pvh/setup.rs
@@ -1,14 +1,20 @@
 use super::*;
-use core::ptr;
 //use x86_64::registers;
 
 #[no_mangle]
 pub extern "C" fn _rust_start(start_info_ptr: *const HvmStartInfo) {
-    if start_info_ptr == ptr::null() {
-        panic!("Invalid PVH start info pointer");
-    }
-    let version = unsafe { (*start_info_ptr).version };
-    let memmap_paddr = unsafe { (*start_info_ptr).memmap_paddr };
     let start_info = HvmStartInfo::new(start_info_ptr);
+    let memmap_paddr = start_info.memmap_paddr as *const HvmMemMapTableEntry;
+    let memmap_entries = start_info.memmap_entries;
+    let memtable = HvmMemMapTable::new(memmap_paddr, memmap_entries);
+    for entry in memtable.into_iter() {
+        match entry.mapping_type {
+            HvmMemmapType::Ram => {
+                let start = entry.addr;
+                let end = entry.addr + entry.size;
+            }
+            _ => (),
+        }
+    }
     crate::__impl_main();
 }
